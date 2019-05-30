9667a909089a221bdc999c8c29b630dbc2957c02
João Abrantes <jpiabrantes@gmail.com>
2019-05-30 14:08:33 +0100

    first commit

--- utils/coma_helper.py
+++ utils/coma_helper.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def get_states_actions_for_locs(state_action, locs, n_rows, n_cols):
+def get_states_actions_for_locs_and_dna(state_action, locs, dnas, n_rows, n_cols, dna_index):
     states_actions = np.empty((len(locs), ) + state_action.shape)
     # center state and action maps on agent
-    for i, (row, col) in enumerate(locs):
+    for i, ((row, col), dna) in enumerate(zip(locs, dnas)):
         states_actions[i] = _center_state_action_on_loc(state_action, row, col, n_rows, n_cols)
+        states_actions[i][..., dna_index] = states_actions[i][..., dna_index] == dna
     # Erase the action of the centered agent
     states_actions[:, n_rows // 2, n_cols // 2, -1] = -1
     return states_actions
