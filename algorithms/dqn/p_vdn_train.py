e20f79653dfc2333cf7cb9ee831b46911ea73b55
João Abrantes <jpiabrantes@gmail.com>
2019-06-20 22:17:17 +0100

    trainers

--- algorithms/dqn/p_vdn_train.py
+++ algorithms/dqn/p_vdn_train.py
@@ -9,6 +9,7 @@ import tensorflow as tf
 from utils.misc import Timer
 from utils.filters import MeanStdFilter, FilterManager
 from algorithms.dqn.sampler import Sampler
+from algorithms.dqn.trainer import Trainer
 
 
 Networks = namedtuple('Networks', ('main', 'target'))
@@ -16,9 +17,9 @@ Weights = namedtuple('Weights', ('main', 'target'))
 
 
 class DQN_trainer:
-    def __init__(self, env_creator,  brain_creator, population_size, batch_size=32, update_freq=4, gamma=0.99,
-                 start_eps=1, end_eps=0.1, annealing_steps=50000, num_epochs=5000, pre_train_steps=250, tau=0.001,
-                 n_samplers=7, num_envs_per_sampler=40, num_of_steps_per_sample=1):
+    def __init__(self, env_creator,  brain_creator, population_size, gamma=0.99,
+                 start_eps=1, end_eps=0.1, annealing_steps=50000, tau=0.001, n_trainers=5,
+                 n_samplers=40, num_envs_per_sampler=7, num_of_steps_per_sample=1, learning_rate=0.0005):
         env = env_creator()
         self.env = env
 
@@ -30,8 +31,8 @@ class DQN_trainer:
         self.weights = {}
         for species_index in range(population_size):
             brain = brain_creator()
-            weights = brain.get_weights()
-            self.weights[species_index] = Weights(weights, weights)
+            main_weights = brain.get_weights()
+            self.weights[species_index] = Weights(main_weights, main_weights)
 
         main_qn = brain_creator()
         target_qn = brain_creator()
@@ -39,17 +40,16 @@ class DQN_trainer:
         self.filters = {'ActorObsFilter': MeanStdFilter(shape=self.env.observation_space.shape)}
         filter_manager = FilterManager()
 
-        species_dict = {species_index: {'steps': 0, 'eps': start_eps,
-                                        'optimiser': tf.keras.optimizers.Adam(learning_rate=0.001)}
+        species_dict = {species_index: {'steps': 0, 'eps': start_eps, 'optimiser_weights': None}
                         for species_index in range(population_size)}
         samplers = [Sampler.remote(env_creator, num_envs_per_sampler, num_of_steps_per_sample,
                                    brain_creator) for _ in range(n_samplers)]
+        trainers = [Trainer.remote(brain_creator, gamma, learning_rate) for _ in range(n_trainers)]
 
         # Set the rate of random action decrease.
 
         train_summary_writer = tf.summary.create_file_writer(tensorboard_folder)
         episodes = 0
-        training_losses = []
         total_steps = 0
         while True:
             total_steps += 1
@@ -60,78 +60,48 @@ class DQN_trainer:
                 species_buffers_list = ray.get([sampler.rollout.remote(weights_id, eps_id) for sampler in samplers])
                 species_buffers = self._concatenate_sampler_results(species_buffers_list)
 
+            training_species_idx = []
+            training_results = []
+            i = 0
             with Timer() as training_time:
                 for species_index, buffer in species_buffers.items():
                     dict_ = species_dict[species_index]
                     dict_['steps'] += len(buffer.buffer)
-                    if dict_['steps'] > pre_train_steps:
-                        if dict_['steps'] > annealing_steps:
-                            coeff = (dict_['steps']-annealing_steps)/annealing_steps
-                            dict_['eps'] = max(coeff * 0.01 + (1 - coeff) * end_eps, 0.01)
-                        else:
-                            coeff = dict_['steps']/annealing_steps
-                            dict_['eps'] = max(coeff*end_eps+(1-coeff)*start_eps, end_eps)
+                    if dict_['steps'] > annealing_steps:
+                        coeff = (dict_['steps']-annealing_steps)/annealing_steps
+                        dict_['eps'] = max(coeff * 0.01 + (1 - coeff) * end_eps, 0.01)
+                    else:
+                        coeff = dict_['steps']/annealing_steps
+                        dict_['eps'] = max(coeff*end_eps+(1-coeff)*start_eps, end_eps)
+                    training_species_idx.append(species_index)
+                    training_results.append(trainers[i].train.remote(self.weights[species_index], buffer.buffer,
+                                                                     species_dict[species_index]['optimiser_weights']))
+                    i = (i + 1) % len(trainers)
+                training_results = ray.get(training_results)
 
-                        '''
-                        steps: list of size batch_size with a list of size n_agents with experiences
-                        '''
-                        steps = buffer.sample_steps(batch_size)  # Get a random batch of experiences.
+            training_losses = []
+            with Timer() as saving_time:
+                for species_index, (main_weights, optimiser_weights, loss) in zip(training_species_idx, training_results):
+                    species_dict[species_index]['optimiser_weights'] = optimiser_weights
+                    training_losses.append(loss)
 
-                        main_qn.set_weights(self.weights[species_index].main)
-                        target_qn.set_weights(self.weights[species_index].target)
-
-                        # Below we perform the Double-DQN update to the target Q-values
-                        list_of_obs = [None]*len(steps)
-                        list_of_act = [None]*len(steps)
-                        target_q = np.zeros((len(steps),), np.float32)
-                        for i, step in enumerate(steps):
-                            # step: list of experiences of size #n_agents
-                            step = np.stack(step)  # n_agents, 5
-                            obs, act, rew, n_obs, done = [np.array(x.tolist(), t)
-                                                          for x, t in zip(step.T,
-                                                                          (np.float32, np.int32, np.float32,
-                                                                           np.float32, np.bool))]
-                            # obs: n_agents, obs_dim
-                            list_of_obs[i] = obs
-                            list_of_act[i] = act
-                            assert len(np.unique(rew)) == 1
-
-                            target_q[i] = rew[0]
-                            alive_mask = np.logical_not(done)
-                            if np.any(alive_mask):
-                                # compute Qtot for alive agents
-                                n_actions = main_qn.get_actions(n_obs[alive_mask], 0)
-                                q_out = target_qn.q.predict(n_obs[alive_mask])
-                                double_q = q_out[range(len(n_actions)), n_actions]
-                                target_q[i] += gamma * np.sum(double_q)
-
-                        # Update the network with our target values.
-                        with tf.GradientTape() as t:
-                            loss = self.td_loss(main_qn(list_of_obs, list_of_act), target_q)
-                        grads = t.gradient(loss, main_qn.variables)
-                        # TODO: give a training loss per species
-                        training_losses.append(loss)
-                        dict_['optimiser'].apply_gradients(zip(grads, main_qn.variables))
-                        #result = main_qn.train_on_batch(x=list_of_obs, y=target_q)
-
-                        target_w = []
-                        for mw, tw in zip(main_qn.get_weights(), target_qn.get_weights()):
-                            target_w.append(tau * mw + (1-tau)*tw)
-                        self.weights[species_index] = Weights(main_qn.get_weights(), target_w)
+                    target_w = []
+                    for mw, tw in zip(main_weights, self.weights[species_index].target):
+                        target_w.append(tau * mw + (1-tau)*tw)
+                    self.weights[species_index] = Weights(main_weights, target_w)
+                    species_folder = os.path.join(exp_folder, str(species_index))
+                    main_qn.save_weights(species_folder)
+                    target_qn.save_weights(species_folder)
 
             # get ep_stats from samplers
-            metrics = {}
+            metrics = {'Episodes': episodes, 'Sampling time': sampling_time.interval,
+                       'Training time': training_time.interval, 'Saving time': saving_time.interval,
+                       'Training loss': np.mean(training_losses)}
 
             ep_metrics = self._concatenate_ep_stats(ray.get([s.get_ep_stats.remote() for s in samplers]))
             if ep_metrics['EpisodesThisIter']:
                 metrics.update(ep_metrics)
                 episodes += ep_metrics['EpisodesThisIter']
-
-            training_loss = 0 if not len(training_losses) else np.mean(training_losses)
-            training_losses = []
-            training_metrics = {'QLoss': training_loss, 'Episodes': episodes, 'Sampling time': sampling_time.interval,
-                                'Training time': training_time.interval}
-            metrics.update(training_metrics)
 
             mean_eps, mean_steps = [], []
             for species_index, dict_ in species_dict.items():
@@ -196,5 +166,5 @@ if __name__ == '__main__':
                 'action_space': env.action_space}
     brain_creator = lambda: VDNMixer(**q_kwargs)
 
-    ray.init()
+    ray.init(local_mode=False)
     trainer = DQN_trainer(env_creator, brain_creator, population_size=5)
